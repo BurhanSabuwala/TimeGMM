e87ab397bebb19775b3e1d75e72126046879c8a8
Burhan Sabuwala <52125885+BurhanSabuwala@users.noreply.github.com>
2023-04-04 13:41:35 -0400

    Add files via upload

--- GMM.py
+++ GMM.py
@@ -360,13 +360,12 @@ def TimeGMM(X, Timepoints, Q,case = "full",tol = 1e-8):
 
   returns - final parameter set : params, predicted data classes, Posterior
   """
-  Timepoints = 
   classes = np.array([0])
   y = np.array([0 for i in range(len(X))])
 
   n_t = {}; d_t = {}
   for t in Timepoints:
-    n,d = np.shape(t)
+    n,d = np.shape(X[t])
     n_t[t] = n
     d_t[t] = d
   #n,d = np.shape(X)
@@ -387,18 +386,21 @@ def TimeGMM(X, Timepoints, Q,case = "full",tol = 1e-8):
     log_prob_a = 0  # log likelihood before update
     log_prob_b = 0  # log likelihood after update
 
-    X_c = X[(y==c).ravel(),:] # Data belonging to class c
-    n_c = X_c.shape[0]  
-    c_mat = np.zeros(d_t[Timepoints[0]],Q)
+    #X_c = X[(y==c).ravel(),:] # Data belonging to class c
+    #n_c = X_c.shape[0]  
+    c_mat = np.zeros((d,Q))
 
-    while (abs(log_prob_a-log_prob_b)>tol or ite == 0) and ite<100:
+    #while (abs(log_prob_a-log_prob_b)>tol or ite == 0) and ite<100:
+    while ite<2:
       #*** Initialization step ***
       if ite == 0:
         time0_params = gmm(X[Timepoints[0]], Q, case) # Data of timepoint 0
         #for each component
         for q in range(Q):
-          params[q]['c'] = time0_params[q]['mu']
+          params[q] = {}
+          params[q]['c'] = time0_params[q]['mu'][:,0]
           c_mat[:,q] = params[q]['c']
+          params[q][Timepoints[0]] = {}
           params[q][Timepoints[0]]['cov'] = time0_params[q]['cov']
           params[q][Timepoints[0]]['w'] = time0_params[q]['w']
 
@@ -406,8 +408,8 @@ def TimeGMM(X, Timepoints, Q,case = "full",tol = 1e-8):
 
       #For each timepoint
       posterior = {}
-      for t in len(1,range(Timepoints)):
-        posterior[Timepoints[t]] = np.zeros((n_t[t], Q))
+      for t in range(1,len(Timepoints)):
+        posterior[Timepoints[t]] = np.zeros((n_t[Timepoints[t]], Q))
         log_prob_b = log_prob_a
         log_prob_a = 0
 
@@ -416,39 +418,38 @@ def TimeGMM(X, Timepoints, Q,case = "full",tol = 1e-8):
         for q in range(Q):
           if ite == 0:
             #Initializing parameters for each class using kmeans
-            v_q = np.zeros(size = len(d_t[Timepoints[t]]))
+            v_q = np.zeros((d_t[Timepoints[t]]))
             mu_q = v_q * Timepoints[t] + params[q]['c']
             #mu_q = init_params[q]['mu']
-            cov_q = init_params[q][Timepoints[t-1]]['cov']
-            w_q = init_params[q][Timepoints[t-1]]['w']
+            params[q][Timepoints[t]] = {}
+            params[q][Timepoints[t]]['cov'] = cov_q = params[q][Timepoints[t-1]]['cov']
+            params[q][Timepoints[t]]['w'] = w_q = params[q][Timepoints[t-1]]['w']
 
           else:
             v_q = params[q]['v']
             cov_q = params[q][Timepoints[t]]['cov']
-            w_q = params[q][Timepoint[t]]['w']
+            w_q = params[q][Timepoints[t]]['w']
 
           min_eig = 1
           det =  np.linalg.det(cov_q)
-
           #Inspecting the Covariance Matrix and enforcing non-singularity and positive definitiveness
           while abs(det)<1e-12 or det<0 or min_eig < 0:
-            cov_q += abs(np.diag((np.random.normal(0,2,(d,1))).ravel()))  ##Addition of Gaussian Noise
+            cov_q += abs(np.diag((np.random.normal(0,0.5,(d,1))).ravel()))  ##Addition of Gaussian Noise
             det = np.linalg.det(cov_q)
             if det > 0:
               min_eig = np.min(np.real(np.linalg.eigvals(cov_q)))         ##Minimum Eigenvalue
           
           icov_q = np.linalg.inv(cov_q)  ##Inverse of Covariance Matrix
-
           #Calculating likelihood given component/cluster
           #X[t] -> (nxd), mu_q -> (dx1), cov_q -> (dxd), finally, tmp -> (nx1)
-          tmp  = np.sum(np.dot((X[Timepoints[t]].T-mu_q).T,icov_q) * (X[Timepoints[t]].T-mu_q).T,axis=1)
+          tmp  = np.sum(np.dot((X[Timepoints[t]].T-mu_q.reshape(-1,1)).T,icov_q) * (X[Timepoints[t]].T-mu_q.reshape(-1,1)).T,axis=1)
           # pX_q_c (log-likelihood) -> (nx1)
           pX_q_c = (1/((2*np.pi)**(d_t[Timepoints[t]]/2))) * (np.linalg.det(cov_q)**-0.5) * np.exp(-0.5*tmp) 
-          
+
           #Enforcing Bounds on the likelihood values to prevent small, large or nan values
-          pX_q_c[pX_q_c<1e-8] = 1e-8
+          pX_q_c[pX_q_c<1e-8] = 1e-10
           pX_q_c[pX_q_c==np.inf] = 1
-          pX_q_c[np.isnan(pX_q_c)] = 1e-8
+          pX_q_c[np.isnan(pX_q_c)] = 1e-10
 
           posterior[Timepoints[t]][:,q] = w_q*pX_q_c
           log_prob_a += w_q*pX_q_c ##(nx1 + ... q Q times) 
@@ -457,34 +458,47 @@ def TimeGMM(X, Timepoints, Q,case = "full",tol = 1e-8):
         log_prob_a = np.sum(np.log(log_prob_a))  ##(nx1)
 
         #Calculating Posterior gamma -> (nxQ)
-        posterior[Timepoints[t]] /= np.sum(posterior[Timepoints[t]],axis=1).reshape(-1,1)
-      
+        posterior[Timepoints[t]] = posterior[Timepoints[t]] / np.sum(posterior[Timepoints[t]],axis=1).reshape(-1,1)
 
       #***Maximization Step - Updating Parameters***
       #w_all -> (1xQ)
-      for t in len(1,range(Timepoints)):
+      v_num1 = np.zeros((Q, d_t[Timepoints[t]]))
+      v_num2 = np.zeros((Q, d_t[Timepoints[t]]))
+      v_den = np.zeros((Q, d_t[Timepoints[t]]))
+      for t in range(1,len(Timepoints)):
         w_all = np.sum(posterior[Timepoints[t]],axis = 0)/n_t[Timepoints[t]]
+        
         # print(w_all)
         #mu_all -> (dxQ)
-        #mu_all = (1/(w_all*n_c))*(np.matmul(posterior[Timepoints[t]].T,X_c)).T
-        v_num1 = np.matmul(posterior[Timepoints[t]].T,X[Timepoint[t]])
-        v_num2 = np.sum(posterior[Timepoints[t]], axis = 0)* params
-        
+        #mu_all = (1/(w_all*n_t[Timepoints[t]]))*(np.matmul(posterior[Timepoints[t]].T,X[Timepoints[t]])).T
+        #print(mu_all)
+
+        # v_num1 -> q x d
+        #print(w_all)
+        v_num1 += (1/(w_all.reshape(-1,1)*n_t[Timepoints[t]])) * (np.matmul(posterior[Timepoints[t]].T,X[Timepoints[t]]))
+        v_num2 += (1/(w_all.reshape(-1,1)*n_t[Timepoints[t]])) * (np.sum(posterior[Timepoints[t]], axis = 0).reshape(-1,1) * c_mat.T)
+        v_den  += (Timepoints[t]/(w_all.reshape(-1,1)*n_t[Timepoints[t]])) * np.sum(posterior[Timepoints[t]], axis = 0).reshape(-1,1) * (np.zeros((Q, d_t[Timepoints[t]]))+ 1)
+      v_updated = (1/v_den) * (v_num1 - v_num2)
+      #print(v_num1, v_num2, v_den, w_all, sep= "|")
+
+      for t in range(1, len(Timepoints)):
         for q in range(Q):
-          params[q] = {}
-          params[q][Timepoints[t]] = {}
+          #params[q] = {}
+          #params[q][Timepoints[t]] = {}
           params[q][Timepoints[t]]['w'] = w_all[q]
-----------------------------------------------------------------------------------
-          params[q]['mu'] = mu_all[:,q].reshape(-1,1)
+          params[q]['v'] = v_updated[q]
+          #params[q]['c'] = c_mat[:,q]
+          mu_q = v_updated[q]*Timepoints[t] + params[q]['c']
           #tmp -> (dxd)
-          tmp  = np.matmul(np.matmul(X_c.T-params[q]['mu'],np.diag(posterior[:,q])),(X_c.T-params[q]['mu']).T)
+          tmp  = np.matmul(np.matmul(X[Timepoints[t]].T-mu_q.reshape(-1,1),np.diag(posterior[Timepoints[t]][:,q])),(X[Timepoints[t]].T-mu_q.reshape(-1,1)).T)
           if case == "diag":
             tmp = np.diag(np.diag(tmp))
 
-          tmp2  = w_all[q]*n_c
+          tmp2  = w_all[q]*n_t[Timepoints[t]]
 
-          params[q]['cov'] = tmp/(tmp2)
-        ite += 1 
+          params[q][Timepoints[t]]['cov'] = tmp/(tmp2)
+      ite += 1 
+        #print(params)
   params['class_support'] = class_support
   params['Q'] = Q
 
